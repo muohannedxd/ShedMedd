210e1c13e689607c2f6a96647158200bc246b9ef
muohannedxd <mohanned.kadache@ensia.edu.dz>
2023-12-01 22:31:10 +0100

    drawer edited

--- lib/screens/Shop/Home.dart
+++ lib/screens/Shop/Home.dart
@@ -40,79 +40,94 @@ class _ShopState extends State<Shop> {
       ChatInbox(),
       Profile(),
     ];
-    List<String> _pageTitles = ['ShedMedd', 'Discover', 'Sell an Item', 'Inbox', 'Profile'];
+    List<String> _pageTitles = [
+      'ShedMedd',
+      'Discover',
+      'Sell an Item',
+      'Inbox',
+      'Profile'
+    ];
     List<bool> _isSubpage = [false, false, true, true, false];
 
     return ScrollConfiguration(
       behavior: BehaviorOfScroll(),
       child: Scaffold(
-        appBar: Bar(_pageTitles[currentPageIndex], _isSubpage[currentPageIndex]),
+        appBar:
+            Bar(_pageTitles[currentPageIndex], _isSubpage[currentPageIndex]),
         drawer: AppDrawer(
           current: currentPageIndex,
         ),
         backgroundColor: CustomColors.bgColor,
         body: _pages[currentPageIndex],
         bottomNavigationBar: Container(
-          height: 80,
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
+            border: Border.all(
+              color: CustomColors.grey,
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24.5),
+              topRight: Radius.circular(24.5),
+            ),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24.0),
+              topRight: Radius.circular(24.0),
+            ),
+            child: SizedBox(
+              height: 80,
+              child: BottomNavigationBar(
+                backgroundColor: CustomColors.bgColor,
+                type: BottomNavigationBarType.fixed,
+                currentIndex: currentPageIndex,
+                selectedFontSize: 16,
+                selectedItemColor: CustomColors.textPrimary,
+                unselectedFontSize: 12,
+                onTap: (int index) {
+                  setState(() {
+                    currentPageIndex = index;
+                  });
+                },
+                items: [
+                  BottomNavigationBarItem(
+                    activeIcon:
+                        Icon(Icons.home, color: CustomColors.textPrimary),
+                    icon: Icon(Icons.home_outlined,
+                        color: CustomColors.textPrimary),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon:
+                        Icon(Icons.search, color: CustomColors.textPrimary),
+                    icon: Icon(Icons.search_outlined,
+                        color: CustomColors.textPrimary),
+                    label: 'Discover',
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon: Icon(Icons.add_box_rounded,
+                        color: CustomColors.textPrimary),
+                    icon: Icon(Icons.add_box_outlined,
+                        color: CustomColors.textPrimary),
+                    label: 'Add',
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon: Icon(Icons.email_rounded,
+                        color: CustomColors.textPrimary),
+                    icon: Icon(Icons.email_outlined,
+                        color: CustomColors.textPrimary),
+                    label: 'Inbox',
+                  ),
+                  BottomNavigationBarItem(
+                    activeIcon: Icon(Icons.person_rounded,
+                        color: CustomColors.textPrimary),
+                    icon: Icon(Icons.person_outline_rounded,
+                        color: CustomColors.textPrimary),
+                    label: 'Profile',
+                  ),
+                ],
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.3), // Shadow color
-                  blurRadius: 8, // Shadow blur radius
-                  offset: Offset(2, 2), // Shadow offset
-                ),
-              ],
-              color: CustomColors.bgColor),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            currentIndex: currentPageIndex,
-            selectedFontSize: 16,
-            selectedItemColor: CustomColors.textPrimary,
-            unselectedFontSize: 12,
-            onTap: (int index) {
-              setState(() {
-                currentPageIndex = index;
-              });
-            },
-            items: [
-              BottomNavigationBarItem(
-                activeIcon: Icon(Icons.home, color: CustomColors.textPrimary),
-                icon:
-                    Icon(Icons.home_outlined, color: CustomColors.textPrimary),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                activeIcon: Icon(Icons.search, color: CustomColors.textPrimary),
-                icon: Icon(Icons.search_outlined,
-                    color: CustomColors.textPrimary),
-                label: 'Discover',
-              ),
-              BottomNavigationBarItem(
-                activeIcon: Icon(Icons.add_box_rounded,
-                    color: CustomColors.textPrimary),
-                icon: Icon(Icons.add_box_outlined,
-                    color: CustomColors.textPrimary),
-                label: 'Add',
-              ),
-              BottomNavigationBarItem(
-                activeIcon:
-                    Icon(Icons.email_rounded, color: CustomColors.textPrimary),
-                icon:
-                    Icon(Icons.email_outlined, color: CustomColors.textPrimary),
-                label: 'Inbox',
-              ),
-              BottomNavigationBarItem(
-                activeIcon:
-                    Icon(Icons.person_rounded, color: CustomColors.textPrimary),
-                icon: Icon(Icons.person_outline_rounded,
-                    color: CustomColors.textPrimary),
-                label: 'Profile',
-              ),
-            ],
+            ),
           ),
         ),
       ),
