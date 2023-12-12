ced7408bc120b022bcc6fc1ffeb0843b2b831b4d
muohannedxd <mohanned.kadache@ensia.edu.dz>
2023-12-12 17:38:11 +0100

    animation to splash screen

--- lib/screens/Splash.dart
+++ lib/screens/Splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shedmedd/screens/Shop/Home.dart';
 import '../constants/customColors.dart';
 
 class Splash extends StatefulWidget {
@@ -11,22 +12,36 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   void initState() {
-    super.initState();
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.pop(context);
-      Navigator.pushNamed(context, '/shop');
-    });
-  }
-
+  super.initState();
+  Future.delayed(Duration(seconds: 2), () {
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        transitionDuration: Duration(seconds: 1),
+        pageBuilder: (_, __, ___) => Shop(currentIndex: 0),
+        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
+          return SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1.0, 0.0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          );
+        },
+      ),
+    );
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: CustomColors.buttonPrimary.withOpacity(0.8),
+      backgroundColor: CustomColors.bgColor,
       body: Center(
         child: Image.asset(
-          'assets/icons/logo_white_large.png',
-          width: 140,
-          height: 140,
+          'assets/icons/logo_large.png',
+          width: 160,
+          height: 160,
+          color: CustomColors.textPrimary.withOpacity(0.86),
         ),
       ),
     );
